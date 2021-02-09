fac8b99a783666cd19b5b6bd2012655d966aa999
shreyas gupta <shreyas.charming@gmail.com>
2021-02-09 12:30:13 +0530

    added user preference box

--- src/App.js
+++ src/App.js
@@ -53,157 +53,261 @@ class App extends Component {
             </p>
           </div>
         </header>
-        {/* <div className="container mt-48">
-          <section className="add-item">
-            <BrowserRouter>
-              <button className="text-white bg-black py-2 px-6 rounded-lg">
-                <Link to="/page1">Next</Link>
-              </button>
-              <Route path="/page1" component={Pageone} />
-            </BrowserRouter>
-          </section>
-        </div> */}
+
         <div className="grid grid-cols-3 gap-6 gap-y-16 my-16">
-          <div className="rounded-lg bg-white shadow-lg p-4">
-            <div className="mb-8">
-              <h2 className="text-xl font-medium mb-4">
-                Question Type 1 (MCQ- 7 options)
-              </h2>
-              <label>
-                How much time in a day do you spend on your smartphone?
-              </label>
+          <div>
+            <div className="rounded-lg bg-white shadow-lg p-4">
+              <div className="mb-8">
+                <h2 className="text-xl font-medium mb-4">
+                  Question Type 1 (MCQ- 7 options)
+                </h2>
+                <label>
+                  How much time in a day do you spend on your smartphone?
+                </label>
+              </div>
+              <div className="flex flex-row rounded-lg p-2 bg-gray-100 border-gray-200 border-2 justify-between">
+                <select
+                  id="mcqstyle"
+                  className=" bg-gray-100 appearance-none"
+                  // onChange={this.handleChange}
+                  // value={this.state.q1options}
+                >
+                  <option value="none" selected disabled hidden>
+                    Select an Option
+                  </option>
+                  <option value="Less than 1 Hour">Less than 1 Hour</option>
+                  <option value="1-2 Hours">1-2 Hours</option>
+                  <option value="2-3 Hours">2-3 Hours</option>
+                  <option value="3-4 Hours">3-4 Hours</option>
+                  <option value="4-5 Hours">4-5 Hours</option>
+                  <option value="5-6 Hours">5-6 Hours</option>
+                  <option value="More than 6 Hours">More than 6 Hours</option>
+                </select>
+                <img
+                  src="https://s2.svgbox.net/octicons.svg?ic=chevron-down-bold&color=000000"
+                  width="22"
+                  height="22"
+                  alt="chevron down"
+                  className="mr-4"
+                ></img>
+              </div>
             </div>
-            <div className="flex flex-row rounded-lg p-2 bg-gray-100 border-gray-200 border-2 justify-between">
-              <select
-                id="mcqstyle"
-                className=" bg-gray-100 appearance-none"
-                // onChange={this.handleChange}
-                // value={this.state.q1options}
-              >
-                <option value="none" selected disabled hidden>
-                  Select an Option
-                </option>
-                <option value="Less than 1 Hour">Less than 1 Hour</option>
-                <option value="1-2 Hours">1-2 Hours</option>
-                <option value="2-3 Hours">2-3 Hours</option>
-                <option value="3-4 Hours">3-4 Hours</option>
-                <option value="4-5 Hours">4-5 Hours</option>
-                <option value="5-6 Hours">5-6 Hours</option>
-                <option value="More than 6 Hours">More than 6 Hours</option>
-              </select>
-              <img
-                src="https://s2.svgbox.net/octicons.svg?ic=chevron-down-bold&color=000000"
-                width="22"
-                height="22"
-                alt="chevron down"
-                className="mr-4"
-              ></img>
-            </div>
+            <select id="q1choice" name="q1choice" className="mt-4">
+              <option value="none" selected disabled hidden>
+                Give your Preference
+              </option>
+              <option class="rank1" value="1st Choice">
+                1st Choice
+              </option>
+              <option class="rank2" value="2nd Choice">
+                2nd Choice
+              </option>
+              <option class="rank3" value="3rd Choice">
+                3rd Choice
+              </option>
+            </select>
           </div>
-          <div className="rounded-lg bg-white shadow-lg p-4">
-            <div className="mb-8">
-              <h2 className="text-xl font-medium mb-4">
-                Question Type 2 (MCQ- 5 options)
-              </h2>
-              <label>
-                How much time in a day do you spend on your smartphone?
-              </label>
+
+          <div>
+            <div className="rounded-lg bg-white shadow-lg p-4">
+              <div className="mb-8">
+                <h2 className="text-xl font-medium mb-4">
+                  Question Type 2 (MCQ- 5 options)
+                </h2>
+                <label>
+                  How much time in a day do you spend on your smartphone?
+                </label>
+              </div>
+              <div className="flex flex-row rounded-lg p-2 bg-gray-100 border-gray-200 border-2 justify-between">
+                <select
+                  id="mcqstyle"
+                  className=" bg-gray-100 appearance-none"
+                  // onChange={this.handleChange}
+                  // value={this.state.q2options}
+                >
+                  <option value="none" selected disabled hidden>
+                    Select an Option
+                  </option>
+                  <option value="Less than 1 Hour">Less than 1 Hour</option>
+                  <option value="1-2 Hours">1-2 Hours</option>
+                  <option value="2-4 Hours">2-4 Hours</option>
+                  <option value="4-6 Hours">4-6 Hours</option>
+                  <option value="More than 6 Hours">More than 6 Hours</option>
+                </select>
+                <img
+                  src="https://s2.svgbox.net/octicons.svg?ic=chevron-down-bold&color=000000"
+                  width="22"
+                  height="22"
+                  alt="chevron down"
+                  className="mr-4"
+                />
+              </div>
             </div>
-            <div className="flex flex-row rounded-lg p-2 bg-gray-100 border-gray-200 border-2 justify-between">
-              <select
-                id="mcqstyle"
-                className=" bg-gray-100 appearance-none"
-                // onChange={this.handleChange}
-                // value={this.state.q2options}
-              >
-                <option value="none" selected disabled hidden>
-                  Select an Option
-                </option>
-                <option value="Less than 1 Hour">Less than 1 Hour</option>
-                <option value="1-2 Hours">1-2 Hours</option>
-                <option value="2-4 Hours">2-4 Hours</option>
-                <option value="4-6 Hours">4-6 Hours</option>
-                <option value="More than 6 Hours">More than 6 Hours</option>
-              </select>
-              <img
-                src="https://s2.svgbox.net/octicons.svg?ic=chevron-down-bold&color=000000"
-                width="22"
-                height="22"
-                alt="chevron down"
-                className="mr-4"
-              />
-            </div>
+
+            <select id="q1choice" name="q1choice" className="mt-4">
+              <option value="none" selected disabled hidden>
+                Give your Preference
+              </option>
+              <option class="rank1" value="1st Choice">
+                1st Choice
+              </option>
+              <option class="rank2" value="2nd Choice">
+                2nd Choice
+              </option>
+              <option class="rank3" value="3rd Choice">
+                3rd Choice
+              </option>
+            </select>
           </div>
-          <div className="rounded-lg bg-white shadow-lg p-4">
-            <h2 className="text-xl font-medium mb-4">
-              Question Type 3 (open-ended text field)
-            </h2>
-            <form>
+
+          <div>
+            <div className="rounded-lg bg-white shadow-lg p-4">
+              <h2 className="text-xl font-medium mb-4">
+                Question Type 3 (open-ended text field)
+              </h2>
+              <form>
+                <div className="mb-8">
+                  <label for="male">
+                    How much time in a day do you spend on your smartphone?
+                  </label>
+                </div>
+                <input
+                  type="text"
+                  className="bg-gray-100 rounded-lg p-2 border-gray-200 border-2"
+                  placeholder="Eg: 2 Hour 35 Minutes"
+                  // onChange={this.handleChange}
+                  // value={this.state.q3options}
+                />
+              </form>
+            </div>
+            <select id="q1choice" name="q1choice" className="mt-4">
+              <option value="none" selected disabled hidden>
+                Give your Preference
+              </option>
+              <option class="rank1" value="1st Choice">
+                1st Choice
+              </option>
+              <option class="rank2" value="2nd Choice">
+                2nd Choice
+              </option>
+              <option class="rank3" value="3rd Choice">
+                3rd Choice
+              </option>
+            </select>
+          </div>
+
+          <div>
+            <div className="rounded-lg bg-white shadow-lg p-4">
+              <h2 className="text-xl font-medium mb-4">
+                Question Type 4 (semi open-ended text field)
+              </h2>
+              <form>
+                <div className="mb-8">
+                  <label for="male">
+                    How much time in a day do you spend on your smartphone?
+                  </label>
+                </div>
+                <div className="flex flex-row items-center">
+                  <label for="hour">Hours</label>
+                  <input
+                    type="text"
+                    className="bg-gray-100 rounded-lg p-2 border-gray-200 border-2 w-10 m-1 mr-4"
+                    placeholder="2"
+                    // onChange={this.handleChange}
+                    // value={this.state.q3options}
+                  />
+                  <label for="minute">Minutes</label>
+                  <input
+                    type="text"
+                    className="bg-gray-100 rounded-lg p-2 border-gray-200 border-2 w-10 m-1"
+                    placeholder="34"
+                    // onChange={this.handleChange}
+                    // value={this.state.q3options}
+                  />
+                </div>
+              </form>
+            </div>
+
+            <select id="q1choice" name="q1choice" className="mt-4">
+              <option value="none" selected disabled hidden>
+                Give your Preference
+              </option>
+              <option class="rank1" value="1st Choice">
+                1st Choice
+              </option>
+              <option class="rank2" value="2nd Choice">
+                2nd Choice
+              </option>
+              <option class="rank3" value="3rd Choice">
+                3rd Choice
+              </option>
+            </select>
+          </div>
+
+          <div>
+            <div className="rounded-lg bg-white shadow-lg p-4">
+              <h2 className="text-xl font-medium mb-4">
+                Question Type 5 (Segmented slider-type input)
+              </h2>
               <div className="mb-8">
                 <label for="male">
                   How much time in a day do you spend on your smartphone?
                 </label>
               </div>
-              <input
-                type="text"
-                className="bg-gray-100 rounded-lg p-2 border-gray-200 border-2"
-                placeholder="Eg: 2 Hour 35 Minutes"
-                // onChange={this.handleChange}
-                // value={this.state.q3options}
+              <Slider
+                min={0}
+                max={120}
+                // value={this.state.q5slidervalue}
+                // onChange={this.onSliderChange}
               />
-            </form>
+            </div>
+            <select id="q1choice" name="q1choice" className="mt-4">
+              <option value="none" selected disabled hidden>
+                Give your Preference
+              </option>
+              <option class="rank1" value="1st Choice">
+                1st Choice
+              </option>
+              <option class="rank2" value="2nd Choice">
+                2nd Choice
+              </option>
+              <option class="rank3" value="3rd Choice">
+                3rd Choice
+              </option>
+            </select>
           </div>
-          <div className="rounded-lg bg-white shadow-lg p-4">
-            <h2 className="text-xl font-medium mb-4">
-              Question Type 4 (semi open-ended text field)
-            </h2>
-            <form>
+
+          <div>
+            <div className="rounded-lg bg-white shadow-lg p-4">
+              <h2 className="text-xl font-medium mb-4">
+                Question Type 6 (Slider-type input)
+              </h2>
               <div className="mb-8">
                 <label for="male">
                   How much time in a day do you spend on your smartphone?
                 </label>
               </div>
-              <div className="flex flex-row items-center">
-                <label for="hour">Hours</label>
-                <input
-                  type="text"
-                  className="bg-gray-100 rounded-lg p-2 border-gray-200 border-2 w-10 m-1 mr-4"
-                  placeholder="2"
-                  // onChange={this.handleChange}
-                  // value={this.state.q3options}
-                />
-                <label for="minute">Minutes</label>
-                <input
-                  type="text"
-                  className="bg-gray-100 rounded-lg p-2 border-gray-200 border-2 w-10 m-1"
-                  placeholder="34"
-                  // onChange={this.handleChange}
-                  // value={this.state.q3options}
-                />
-              </div>
-            </form>
-          </div>
-          <div className="rounded-lg bg-white shadow-lg p-4">
-            <h2 className="text-xl font-medium mb-4">
-              Question Type 5 (Segmented slider-type input)
-            </h2>
-            <Slider
-              min={0}
-              max={120}
-              // value={this.state.q5slidervalue}
-              // onChange={this.onSliderChange}
-            />
-          </div>
-          <div className="rounded-lg bg-white shadow-lg p-4">
-            <h2 className="text-xl font-medium mb-4">
-              Question Type 6 (Slider-type input)
-            </h2>
-            <Slider
-              min={0}
-              max={120}
-              // value={this.state.q5slidervalue}
-              // onChange={this.onSliderChange}
-            />
+              <Slider
+                min={0}
+                max={120}
+                // value={this.state.q5slidervalue}
+                // onChange={this.onSliderChange}
+              />
+            </div>
+            <select id="q1choice" name="q1choice" className="mt-4">
+              <option value="none" selected disabled hidden>
+                Give your Preference
+              </option>
+              <option class="rank1" value="1st Choice">
+                1st Choice
+              </option>
+              <option class="rank2" value="2nd Choice">
+                2nd Choice
+              </option>
+              <option class="rank3" value="3rd Choice">
+                3rd Choice
+              </option>
+            </select>
           </div>
         </div>
       </div>
