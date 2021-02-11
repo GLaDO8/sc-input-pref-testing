c496798890f58937216844bdc2f00d835803a17e
shreyas gupta <shreyas.charming@gmail.com>
2021-02-11 10:13:10 +0530

    removed tooltip, added fixed height

--- src/Components/Slidertype1.js
+++ src/Components/Slidertype1.js
@@ -1,9 +1,7 @@
 import React, { Component } from "react";
-import Slider, { createSliderWithTooltip } from "rc-slider";
+import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
 import "./../index.css";
-
-const SliderWithTooltip = createSliderWithTooltip(Slider);
 
 function screenTimeFormatter(v) {
   let hours = Math.floor(v / 60);
@@ -21,23 +19,18 @@ class Slidertype1 extends Component {
     return (
       <div>
         <div className="rounded-lg bg-white shadow-lg p-4">
-          <h2 className="text-xl font-medium mb-4">
-            Question Type 5 (Segmented slider-type input)
-          </h2>
-          <div className="mb-8">
-            <label for="male">
-              How much time in a day do you spend on your smartphone?
-            </label>
-          </div>
+          <h2 className="text-xl font-medium mb-4">Slider with Step</h2>
+          <p className="mb-8">
+            How much time in a day do you spend on your smartphone?
+          </p>
           <div>
-            <p className="text-xl mb-8">{screenTimeFormatter(sliderValue)}</p>
-            <SliderWithTooltip
+            <p className="text-xl mb-6">{screenTimeFormatter(sliderValue)}</p>
+            <Slider
               min={0}
               max={720}
-              step={5}
-              tipFormatter={screenTimeFormatter}
-              tipProps={{ overlayClassName: "foo" }}
+              step={15}
               onChange={this.handleChange}
+              className="mb-6"
             />
           </div>
         </div>
--- src/Components/Slidertype2.js
+++ src/Components/Slidertype2.js
@@ -1,9 +1,7 @@
 import React, { Component } from "react";
-import Slider, { createSliderWithTooltip } from "rc-slider";
+import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
 import "./../index.css";
-
-const SliderWithTooltip = createSliderWithTooltip(Slider);
 
 function screenTimeFormatter(v) {
   let hours = Math.floor(v / 60);
@@ -21,21 +19,16 @@ class Slidertype2 extends Component {
     return (
       <div>
         <div className="rounded-lg bg-white shadow-lg p-4">
-          <h2 className="text-xl font-medium mb-4">
-            Question Type 6 (Slider-type input)
-          </h2>
-          <div className="mb-8">
-            <label for="male">
-              How much time in a day do you spend on your smartphone?
-            </label>
-          </div>
-          <p className="text-xl mb-8">{screenTimeFormatter(sliderValue)}</p>
-          <SliderWithTooltip
+          <h2 className="text-xl font-medium mb-4">Normal Slider</h2>
+          <p className="mb-8">
+            How much time in a day do you spend on your smartphone?
+          </p>
+          <p className="text-xl mb-6">{screenTimeFormatter(sliderValue)}</p>
+          <Slider
             min={0}
             max={720}
-            tipFormatter={screenTimeFormatter}
-            tipProps={{ overlayClassName: "foo" }}
             onChange={this.handleChange}
+            className="mb-6"
           />
         </div>
       </div>
