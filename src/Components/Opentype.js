3d4dd95b778037b276d7d1bc1d6d32ba4503f397
shreyas gupta <shreyas.charming@gmail.com>
2021-02-11 10:13:28 +0530

    updated title, modified options

--- src/Components/Opentype.js
+++ src/Components/Opentype.js
@@ -6,21 +6,22 @@ class Opentype extends Component {
     return (
       <div>
         <div className="rounded-lg bg-white shadow-lg p-4">
+          <h2 className="text-xl font-medium mb-4">Open Ended Style</h2>
           <form>
             <div className="mb-8">
-              <label for="male">
-                How much time in a day do you spend on your smartphone?
-              </label>
+              <p>How much time in a day do you spend on your smartphone?</p>
             </div>
             <div className="flex flex-row items-center">
               <label for="hour">Hours</label>
               <input
+                id="hour"
                 type="text"
                 className="bg-gray-100 rounded-lg p-2 border-gray-200 border-2 w-10 m-1 mr-4"
                 placeholder="2"
               />
               <label for="minute">Minutes</label>
               <input
+                id="minute"
                 type="text"
                 className="bg-gray-100 rounded-lg p-2 border-gray-200 border-2 w-10 m-1"
                 placeholder="34"
